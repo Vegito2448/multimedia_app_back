f9902e29397d89ad347f7de723a6b2ede9fed3c8
Jesús Peña <jesusdpeaj18102001@gmail.com>
2023-01-23 20:04:43 -0400

    users CRUD ready

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -10,10 +10,12 @@ const emailExists = async(mail = '')=>{
 	const existsEmail = await User.findOne({mail});
   if(existsEmail) throw new Error(`Email ${mail} already exists in DB or is empty`);
 };
-const existsUserById = async(id)=>{
+const existsUserById = async (_id) => {
+  console.log("🚀 ~ file: db-validators.js:14 ~ existsUserById ~ id", _id)
+
 //verified if mail exist
-	const existsUser = await User.findOne({id});
-  if(!existsUser) throw new Error(`id: ${id} do not exists`);
+  const existsUser = await User.findOne({ _id });
+  if (!existsUser) throw new Error(`id: ${_id} do not exists or is not Valid`);
 };
 const isANumber = async (number) => {
   if (isNaN(number) && !Number.isInteger()) throw new Error(`Number: ${number} provider is not valid`);
